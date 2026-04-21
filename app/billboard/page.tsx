dff84acdfdac6f780b6ac9a8bb5baf5dd2fadd52
Hammad Wakeel <hammadshah71200@gmail.com>
2026-04-21 23:15:37 +0500

    added website name and icon

--- app/billboard/page.tsx
+++ app/billboard/page.tsx
@@ -10,7 +10,7 @@ export default function BillboardPage() {
 
   const fetchSignal = async () => {
     try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/videos/billboard/active`);
+      const res = await fetch(`https://wajeehaaa-digitalbillboard.hf.space/videos/billboard/active`);
       if (!res.ok) throw new Error("SIGNAL_INTERRUPTED");
       const data = await res.json();
       setActiveAds(data);
